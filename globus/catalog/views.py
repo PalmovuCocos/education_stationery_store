4e9f6f9aa8a19c991e4f78c075f7fe379c2e4012
PalmovuCocos <cocos.barchuck@yandex.ru>
2022-11-08 15:32:51 +0300

    add css for shop.html

--- globus/catalog/views.py
+++ globus/catalog/views.py
@@ -29,7 +29,7 @@ class Shop(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
+        context['title'] = 'Каталог'
         return context
 
 
