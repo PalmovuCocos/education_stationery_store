ade091a9b164d9a78fe7abfe8dbdabd82d131920
PalmovuCocos <cocos.barchuck@yandex.ru>
2022-11-14 23:52:19 +0300

    edit basket and add profile

--- globus/catalog/urls.py
+++ globus/catalog/urls.py
@@ -4,11 +4,12 @@ from .views import *
 urlpatterns = [
     path('', Index.as_view(), name="home"),
     path('shop/', Shop.as_view(), name="shop"),
-    path('basket/', basket, name="basket"),
+    path('basket/', BasketShow.as_view(), name="basket"),
     path('shop/product/<int:product_id>/', Show_product.as_view(), name='show_product'),
     path('authorization', LoginUser.as_view(), name='authorization'),
     path('logout', logout_user, name='logout'),
     path('register', RegisterUser.as_view(), name='register'),
+    path('profile', OrderList.as_view(), name='profile'),
 
     path('api/v1/products', ProductAPIView.as_view(), name='productAPI')
 ]
